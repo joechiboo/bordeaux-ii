f8a56ea8e0b926fd5eaddca0bf43babb29aa60a1
UCL\joechiboo <joechiboo@ucl.com.tw>
2024-05-07 14:21:57 +0800

    navbar bug fix

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,7 +1,7 @@
 <template>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<div class="container-fluid">
-			<router-link class="navbar-brand" to="/welcome">波爾多 - 樂菲莊園</router-link>
+			<router-link class="navbar-brand" to="/welcome" @click.native="collapseNavbar">波爾多 - 樂菲莊園 {{ subtitle }}</router-link>
 			<button
 				class="navbar-toggler"
 				type="button"
@@ -16,19 +16,19 @@
 			<div class="collapse navbar-collapse" id="navbarNav">
 				<ul class="navbar-nav">
 					<li class="nav-item">
-						<router-link class="nav-link" to="/members">委員名單</router-link>
+						<router-link class="nav-link" to="/members" @click.native="collapseNavbar">委員名單</router-link>
 					</li>
 					<li class="nav-item">
-						<router-link class="nav-link" to="/rules">社區規章</router-link>
+						<router-link class="nav-link" to="/rules" @click.native="collapseNavbar">社區規章</router-link>
 					</li>
 					<li class="nav-item">
-						<router-link class="nav-link" to="/events">大事記</router-link>
+						<router-link class="nav-link" to="/events" @click.native="collapseNavbar">大事記</router-link>
 					</li>
 					<li class="nav-item">
-						<router-link class="nav-link" to="/records">會議記錄</router-link>
+						<router-link class="nav-link" to="/records" @click.native="collapseNavbar">會議記錄</router-link>
 					</li>
 					<li class="nav-item">
-						<router-link class="nav-link" to="/facilities">公共設施</router-link>
+						<router-link class="nav-link" to="/facilities" @click.native="collapseNavbar" :class="{ disabled: isFacilitiesDisabled }">公共設施</router-link>
 					</li>
 				</ul>
 			</div>
@@ -37,8 +37,55 @@
 </template>
 
 <script>
+	import { Collapse } from 'bootstrap';
 	export default {
 		name: 'AppNavbar',
+		data() {
+			return {
+				subtitle: '',
+				isFacilitiesDisabled: true, // 根据实际情况设置
+			};
+		},
+		created() {
+			this.setSubtitle(this.$route.path);
+			this.$router.beforeEach((to, from, next) => {
+				this.setSubtitle(to.path);
+				next();
+			});
+		},
+		methods: {
+			collapseNavbar() {
+				const navbarCollapse = document.getElementById('navbarNav');
+				const bsCollapse = new Collapse(navbarCollapse, {
+					toggle: false,
+				});
+				bsCollapse.hide();
+			},
+			setSubtitle(path) {
+				switch (path) {
+					case '/welcome':
+						this.subtitle = '';
+						break;
+					case '/members':
+						this.subtitle = '委員名單';
+						break;
+					case '/rules':
+						this.subtitle = '社區規章';
+						break;
+					case '/events':
+						this.subtitle = '大事記';
+						break;
+					case '/records':
+						this.subtitle = '會議記錄';
+						break;
+					case '/facilities':
+						this.subtitle = '公共設施';
+						break;
+					default:
+						this.subtitle = ''; // 當路徑不匹配時無副標題
+				}
+			},
+		},
 	};
 </script>
 
