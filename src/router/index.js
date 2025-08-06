6603d1a7505c3441f030fe7426cb6067e8358481
UCL\joechiboo <joechiboo@ucl.com.tw>
2025-08-06 12:54:28 +0800

    Fix router base path for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -53,7 +53,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHistory(process.env.NODE_ENV === 'production' ? '/bordeaux-ii/' : '/'),
 	routes,
 });
 
